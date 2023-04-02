0446018fcedf34026ef0abedd229d687e0e51221
Nikolai Kochuk <ganortosys@gamil.com>
2023-04-02 17:46:47 +0500

    Create geometry

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.glCanvas = new OpenTK.GLControl();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // glCanvas
+            // 
+            this.glCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.glCanvas.BackColor = System.Drawing.Color.Black;
+            this.glCanvas.Location = new System.Drawing.Point(231, 3);
+            this.glCanvas.Name = "glCanvas";
+            this.glCanvas.Size = new System.Drawing.Size(575, 461);
+            this.glCanvas.TabIndex = 0;
+            this.glCanvas.VSync = false;
+            this.glCanvas.Load += new System.EventHandler(this.OnLoad);
+            this.glCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 28.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 71.75F));
+            this.tableLayoutPanel1.Controls.Add(this.glCanvas, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(809, 467);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 461);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(809, 467);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Демо Куб";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private OpenTK.GLControl glCanvas;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,15 +6,136 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
 
 namespace DemoCube
 {
     public partial class Form1 : Form
     {
+        Cube cube;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            GL.Viewport(0, 0, glCanvas.Width, glCanvas.Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            glCanvas.SwapBuffers();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.Cyan);
+            cube = new Cube();
+        }
+
+        private void OnClosing(object sender, FormClosingEventArgs e)
+        {
+            cube.FreeBuffers();
+        }
     }
+
+    public class Cube
+    {
+        const float side = 10.0f;
+        const float initAngle = 7 * (float)Math.PI / 4;
+        const float offsetAngle = (float)Math.PI / 2;
+        const int totalPoints = 6 * 4;
+        int vao;
+        int vbo;
+        int ebo;
+        public Cube(Vector3 origin = new Vector3())
+        {
+            var points = new List<Vector3>(totalPoints * 2);
+            var indices = new List<uint>(totalPoints + 12);
+            var up = new List<Vector3>(4);
+            var down = new List<Vector3>(4);
+            var faces = new List<Vector3>(16);
+            CreateFaces(ref origin, up, down, faces);
+            FillFaces(up, points, indices);
+            FillFaces(down, points, indices, 0, 4);
+            FillFaces(faces, points, indices, 0, 8);
+            vao = GL.GenVertexArray();
+            vbo = GL.GenBuffer();
+            ebo = GL.GenBuffer();
+            GL.BindVertexArray(vao);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, points.Count * sizeof(float) * 3, points.ToArray(), BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData<uint>(BufferTarget.ElementArrayBuffer, indices.Count * sizeof(uint),indices.ToArray(), BufferUsageHint.StreamDraw);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, sizeof(float) * 6, 0);
+            GL.EnableVertexAttribArray(0);
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, sizeof(float) * 6, 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+        }
+
+        public void FreeBuffers()
+        {
+            GL.BindVertexArray(0);
+            GL.DeleteVertexArray(vao);
+            GL.DeleteBuffer(vbo);
+            GL.DeleteBuffer(ebo);
+            vao = vbo = 0;
+        }
+
+        private void CreateFace(List<Vector3> points, ref Vector3 origin, float offsetAngle, float radius)
+        {
+            var vector = new Vector3(0.0f, origin.Y, 0.0f);
+            float angle = initAngle;
+            for (var i = 0; i < 4; ++i)
+            {
+                vector.X = origin.X + radius * (float)Math.Cos(angle);
+                vector.Z = origin.Z + radius * -(float)Math.Sin(angle);
+                points.Add(vector);
+                angle += offsetAngle;
+            }
+        }
+
+        private void CreateFaces(ref Vector3 origin, List<Vector3> up, List<Vector3> down, List<Vector3> faces)
+        {
+            origin.Y = origin.Y + side / 2;
+            var radius = (float)Math.Sqrt(2) * side / 2;
+            CreateFace(up, ref origin, offsetAngle, radius);
+            origin.Y = -origin.Y;
+            CreateFace(down, ref origin, -offsetAngle, radius);
+            for(int i = 0, j = 3; i < 4; ++i, --j)
+            {
+                faces.Add(up[i]);
+                faces.Add(i == 0 ? down[0] : down[j + 1]);
+                faces.Add(down[j]);
+                faces.Add(i == 3 ? up[0] : up[i + 1]);
+            }
+        }
+
+        private void FillFaces(List<Vector3> src, List<Vector3> points, List<uint> indices, int srcRead = 0, int dstWrite = 0)
+        {
+            var totalSides = (src.Count - srcRead) / 4;
+            for (var i = 0; i < totalSides; ++i, srcRead += 4, dstWrite += 4)
+            {
+                var normal = GetNormal(src[srcRead + 1] - src[srcRead], src[srcRead + 2] - src[srcRead]);
+                for (var j = 0; j < 4; ++j)
+                {
+                    points.Add(src[srcRead + j]);
+                    points.Add(normal);
+                    indices.Add((uint)(dstWrite + j));
+                    if (j == 2)
+                        indices.Add((uint)(dstWrite + j));
+                }
+                indices.Add((uint)dstWrite);
+            }
+        }
+
+        private Vector3 GetNormal(Vector3 left, Vector3 right)
+        {
+            var normal = Vector3.Cross(left, right);
+            normal.Normalize();
+            return normal;
+        }
+    }
+
 }
